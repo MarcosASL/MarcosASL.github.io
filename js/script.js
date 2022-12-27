309a8b54c4b3408923e220dfdccdbc8132623ce6
Marcos ASL <Mark.ASL@hotmail.com>
2022-12-27 12:03:58 -0300

    test

--- js/script.js
+++ js/script.js
@@ -23,3 +23,15 @@ function clickMenu() {
     navegacaolista.style.display = "block";
   }
 }
+
+/*SELECTOR DE LINKS ON*/
+// const links = document.querySelectorAll("nav a");
+// function ativarLink(link) {
+//   const href = link.href;
+//   const url = document.location.href;
+//   if (href === url) {
+//     link.style.backgroundColor = "black";
+//     link.style.color = "white";
+//   }
+// }
+// links.forEach(ativarLink);
